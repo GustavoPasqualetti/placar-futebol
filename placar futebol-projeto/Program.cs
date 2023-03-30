5ccbfd88873889947cde3b1b2f546de8f2b63dcc
Gustavo Pasqualetti <49802004863@info.local>
2023-03-30 10:26:53 -0300

    placar de futebol

---
+++ placar futebol-projeto/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine($"Infome quantos gols o primeiro time fez");
+int time1= int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Infome quantos gols o segundo time fez");
+int time2= int.Parse(Console.ReadLine());
+
+if (time1 > time2)
+{
+    Console.WriteLine($"A vitoria foi do primeiro time");
+    
+}
+
+else if (time1 == time2)
+{
+Console.WriteLine($"O jogo acabou empatado");
+
+}
+
+else
+{
+    Console.WriteLine($"A vitoria foi do segundo time");
+    
+}
+
